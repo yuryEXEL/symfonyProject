c9fc5580359f4fe05c4e58adc2130165ad0a065d
Yury <Uleichyk>
2024-01-08 14:28:53 +0300

    added user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\ArrayShape;
+
+#[ORM\Table(name: '`user`')]
+#[ORM\Entity]
+class User
+{
+    #[ORM\Column(name: 'id', type: 'bigint', unique: true)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false)]
+    private string $login;
+
+    #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false)]
+    private DateTime $createdAt;
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime', nullable: false)]
+    private DateTime $updatedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): void {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(): void {
+        $this->updatedAt = new DateTime();
+    }
+
+    #[ArrayShape(['id' => 'int|null', 'login' => 'string', 'createdAt' => 'string', 'updatedAt' => 'string'])]
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'login' => $this->login,
+            'createdAt' => $this->createdAt->format('Y-m-d H:i:s'),
+            'updatedAt' => $this->updatedAt->format('Y-m-d H:i:s'),
+        ];
+    }
+}
---
+++ src/Manager/UserManager.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserManager
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    ) {
+    }
+
+    public function create(string $login): User
+    {
+        $user = new User();
+        $user->setLogin($login);
+        $user->setCreatedAt();
+        $user->setUpdatedAt();
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+}
